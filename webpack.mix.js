d1ea658c148f5c12f7e52b227fbaa8b0d52454c0
RJS <premonday@gmail.com>
2020-12-06 13:04:44 +0100

    add copy directory resources to public

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,7 @@ require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/css/app.scss', 'public/css')
+  .copyDirectory('resources/img', 'public/img')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./tailwind.config.js'), postcssColorMod() ],
